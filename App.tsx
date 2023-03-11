03c224539579d3eb3a41d3757a12a8dc73d78293
Memo Cachú <gcachu.o@gmail.com>
2023-03-10 18:54:15 -0600

    Check for updates

--- App.tsx
+++ App.tsx
@@ -1,20 +1,35 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { useEffect } from "react";
+import * as Updates from 'expo-updates';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+
+    useEffect(() => {
+        async function checkForUpdate() {
+            try {
+                const { isAvailable } = await Updates.checkForUpdateAsync();
+                if (isAvailable) {
+                    await Updates.fetchUpdateAsync();
+                    // Recarga la aplicación para cargar la actualización
+                    await Updates.reloadAsync();
+                }
+            } catch (e) {
+                console.log(e);
+            }
+        }
+
+        checkForUpdate();
+    }, []);
+
+    return (<View style={styles.container}>
+        <Text>Open up App.tsx to start working on your app!</Text>
+        <StatusBar style="auto" />
+    </View>);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1, backgroundColor: '#fff', alignItems: 'center', justifyContent: 'center',
+    },
 });
