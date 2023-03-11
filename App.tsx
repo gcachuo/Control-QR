a0814fbe352a4393c328b3426b6434b5d653d924
Memo Cachú <gcachu.o@gmail.com>
2023-03-10 19:23:40 -0600

    prettier

--- App.tsx
+++ App.tsx
@@ -1,35 +1,39 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
 import { useEffect } from "react";
-import * as Updates from 'expo-updates';
+import * as Updates from "expo-updates";
 
 export default function App() {
-
-    useEffect(() => {
-        async function checkForUpdate() {
-            try {
-                const { isAvailable } = await Updates.checkForUpdateAsync();
-                if (isAvailable) {
-                    await Updates.fetchUpdateAsync();
-                    // Recarga la aplicación para cargar la actualización
-                    await Updates.reloadAsync();
-                }
-            } catch (e) {
-                console.log(e);
-            }
+  useEffect(() => {
+    async function checkForUpdate() {
+      try {
+        const { isAvailable } = await Updates.checkForUpdateAsync();
+        if (isAvailable) {
+          await Updates.fetchUpdateAsync();
+          // Recarga la aplicación para cargar la actualización
+          await Updates.reloadAsync();
         }
+      } catch (e) {
+        console.log(e);
+      }
+    }
 
-        checkForUpdate();
-    }, []);
+    checkForUpdate();
+  }, []);
 
-    return (<View style={styles.container}>
-        <Text>Open up App.tsx to start working on your app!</Text>
-        <StatusBar style="auto" />
-    </View>);
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.tsx to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1, backgroundColor: '#fff', alignItems: 'center', justifyContent: 'center',
-    },
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
 });
