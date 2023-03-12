793c79f3ef7ba78e615f5fd2f01364a631cce5da
Memo Cachú <gcachu.o@gmail.com>
2023-03-12 16:00:44 -0600

    Save phone number

--- screens/MyAccountScreen.tsx
+++ screens/MyAccountScreen.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { StyleSheet, TouchableWithoutFeedback, View } from "react-native";
 import {
   Avatar,
@@ -12,6 +12,7 @@ import {
 import BackButton from "../components/BackButton";
 import { useAuth } from "../hooks/useAuth";
 import { updateProfile } from "firebase/auth/react-native";
+import { doc, getDoc, getFirestore, setDoc } from "firebase/firestore";
 
 export default function MyAccountScreen() {
   const { user } = useAuth();
@@ -21,6 +22,17 @@ export default function MyAccountScreen() {
     email: "",
     phoneNumber: "",
   });
+  const [phoneNumber, setPhoneNumber] = useState("");
+
+  useEffect(() => {
+    (async () => {
+      if (user) {
+        const data = await getDoc(doc(getFirestore(), "users", user.uid));
+        console.log(data.data());
+        setPhoneNumber(data.data()?.phoneNumber);
+      }
+    })();
+  }, [user]);
 
   const handleSave = async () => {
     try {
@@ -28,6 +40,10 @@ export default function MyAccountScreen() {
         console.log("Start saving user");
 
         await updateProfile(user, editedData);
+        await setDoc(doc(getFirestore(), "users", user.uid), {
+          phoneNumber: editedData.phoneNumber,
+        });
+        setPhoneNumber(editedData.phoneNumber);
 
         console.log("Finish save");
       }
@@ -51,14 +67,14 @@ export default function MyAccountScreen() {
           {user?.displayName || "<Sin Nombre>"}
         </Title>
         <Caption style={styles.caption}>{user?.email || ""}</Caption>
-        <Caption style={styles.caption}>{user?.phoneNumber || ""}</Caption>
+        <Caption style={styles.caption}>{phoneNumber || ""}</Caption>
 
         <TouchableWithoutFeedback
           onPress={() => {
             setEditedData({
               displayName: user?.displayName || "",
               email: user?.email || "",
-              phoneNumber: user?.phoneNumber || "",
+              phoneNumber: phoneNumber || "",
             });
             setIsModalVisible(true);
           }}
@@ -89,9 +105,9 @@ export default function MyAccountScreen() {
             <TextInput
               label="Teléfono"
               value={editedData.phoneNumber}
-              onChangeText={(text) =>
-                setEditedData({ ...editedData, phoneNumber: text })
-              }
+              onChangeText={(text) => {
+                setEditedData({ ...editedData, phoneNumber: text });
+              }}
             />
             <Button
               onPress={async () => {
