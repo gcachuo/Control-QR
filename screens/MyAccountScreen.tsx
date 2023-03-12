27699a1ec9c6ab4ac6b8fbf21528bf0fea555733
Memo Cachú <gcachu.o@gmail.com>
2023-03-12 15:03:40 -0600

    Use firestore

--- screens/MyAccountScreen.tsx
+++ screens/MyAccountScreen.tsx
@@ -1,20 +1,109 @@
-import React from "react";
-import { StyleSheet, View } from "react-native";
-import { Avatar, Caption, Title } from "react-native-paper";
+import React, { useState } from "react";
+import { StyleSheet, TouchableWithoutFeedback, View } from "react-native";
+import {
+  Avatar,
+  Button,
+  Caption,
+  IconButton,
+  Modal,
+  TextInput,
+  Title,
+} from "react-native-paper";
 import BackButton from "../components/BackButton";
+import { useAuth } from "../hooks/useAuth";
+import { doc, getFirestore, setDoc } from "firebase/firestore";
 
 export default function MyAccountScreen() {
+  const { user } = useAuth();
+  const [isModalVisible, setIsModalVisible] = useState(false);
+  const [editedData, setEditedData] = useState({
+    displayName: "",
+    email: "",
+    phoneNumber: "",
+  });
+
+  const handleSave = async () => {
+    try {
+      if (user) {
+        console.log("Start saving user");
+
+        await setDoc(doc(getFirestore(), "users", user.uid), editedData);
+
+        console.log("Finish save");
+      }
+    } catch (error) {
+      console.warn(error);
+    }
+  };
+
   return (
     <View>
       <BackButton />
       <View style={styles.container}>
         <Avatar.Image
-          source={{ uri: "https://picsum.photos/id/1005/200/200" }}
+          source={{
+            uri: user?.photoURL || "https://picsum.photos/id/1005/200/200",
+          }}
           size={100}
           style={styles.avatar}
         />
-        <Title style={styles.title}>John Doe</Title>
-        <Caption style={styles.caption}>johndoe@example.com</Caption>
+        <Title style={styles.title}>
+          {user?.displayName || "<Sin Nombre>"}
+        </Title>
+        <Caption style={styles.caption}>{user?.email || ""}</Caption>
+
+        <TouchableWithoutFeedback
+          onPress={() => {
+            setEditedData({
+              displayName: user?.displayName || "",
+              email: user?.email || "",
+              phoneNumber: "",
+            });
+            setIsModalVisible(true);
+          }}
+        >
+          <View>
+            <IconButton icon="pencil" size={24} />
+          </View>
+        </TouchableWithoutFeedback>
+        <Modal
+          visible={isModalVisible}
+          onDismiss={() => setIsModalVisible(false)}
+        >
+          <View style={styles.modalContainer}>
+            <TextInput
+              label="Nombre Completo"
+              value={editedData.displayName}
+              onChangeText={(text) =>
+                setEditedData({ ...editedData, displayName: text })
+              }
+            />
+            <TextInput
+              label="Email"
+              value={editedData.email}
+              onChangeText={(text) =>
+                setEditedData({ ...editedData, email: text })
+              }
+            />
+            <TextInput
+              label="Teléfono"
+              value={editedData.phoneNumber}
+              onChangeText={(text) =>
+                setEditedData({ ...editedData, phoneNumber: text })
+              }
+            />
+            <Button
+              onPress={async () => {
+                // Lógica para guardar los datos editados
+                await handleSave();
+                setIsModalVisible(false);
+              }}
+            >
+              Guardar
+            </Button>
+            <Button onPress={() => setIsModalVisible(false)}>Cancelar</Button>
+          </View>
+        </Modal>
       </View>
     </View>
   );
@@ -36,5 +125,13 @@ const styles = StyleSheet.create({
     fontSize: 16,
     lineHeight: 16,
     marginBottom: 10,
+  },
+  modalContainer: {
+    backgroundColor: "white",
+    padding: 20,
+    borderRadius: 8,
+  },
+  textInput: {
+    marginVertical: 8,
   },
 });
