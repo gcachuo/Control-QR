160abdf62123cefcd38522fdab7e890a0d953f32
Memo Cachú <gcachu.o@gmail.com>
2023-03-13 20:42:38 -0600

    Share QR Code

--- App.tsx
+++ App.tsx
@@ -1,6 +1,7 @@
 import "react-native-gesture-handler";
 import "@react-native-async-storage/async-storage";
 import "./firebaseConfig";
+import "moment/locale/es";
 
 import React from "react";
 import { SafeAreaView } from "react-native";
@@ -8,6 +9,9 @@ import DrawerNavigator from "./navigation/DrawerNavigator";
 import { NavigationContainer } from "@react-navigation/native";
 import { AuthProvider } from "./hooks/useAuth";
 import { useAppUpdate } from "./hooks/useAppUpdate";
+import moment from "moment";
+
+moment().locale("es");
 
 export default function App() {
   useAppUpdate();
--- screens/CreateQRScreen.tsx
+++ screens/CreateQRScreen.tsx
@@ -1,24 +1,54 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import { StyleSheet, View } from "react-native";
 import { Button, Text, TextInput } from "react-native-paper";
+import * as Sharing from "expo-sharing";
 import QRCode from "react-native-qrcode-svg";
 import { createQRToken } from "../utils/qrUtils";
 import { useAuth } from "../hooks/useAuth";
+import moment from "moment";
+import ViewShot from "react-native-view-shot";
 
 const CreateQRScreen = () => {
+  const captureQR = useRef<ViewShot>(null);
   const { user } = useAuth();
   const [guestName, setGuestName] = useState("");
-  const [qrValue, setQRValue] = useState("");
+  const [qrValue, setQRValue] = useState<{
+    guestName?: string;
+    createdTime?: number;
+  }>({});
+
+  const isShown = qrValue.guestName;
 
   const handleGenerateQR = () => {
+    setQRValue({});
+
+    if (!guestName) {
+      return;
+    }
+
     const qrData = {
       guestName: guestName,
       creatorUid: user?.uid,
       createdTime: new Date().getTime(),
       type: "unique",
-      token: createQRToken,
+      token: createQRToken(),
     };
-    setQRValue(JSON.stringify(qrData));
+    setQRValue(qrData);
+
+    console.log(qrValue);
+  };
+
+  const onCapture = async () => {
+    if (captureQR.current !== null && captureQR.current !== undefined) {
+      // @ts-ignore
+      const uri = await captureQR.current.capture();
+      if (uri) {
+        await Sharing.shareAsync(uri, {
+          mimeType: "image/png",
+          dialogTitle: "Compartir QR",
+        });
+      }
+    }
   };
 
   return (
@@ -33,11 +63,19 @@ const CreateQRScreen = () => {
         <Button mode="contained" onPress={handleGenerateQR}>
           Generar QR
         </Button>
-        {qrValue ? (
-          <View style={styles.qrContainer}>
-            <QRCode value={qrValue} size={250} />
-            <Text style={styles.qrValue}>{qrValue}</Text>
-          </View>
+        {isShown ? (
+          <>
+            <ViewShot ref={captureQR}>
+              <View style={styles.qrContainer}>
+                <QRCode value={JSON.stringify(qrValue)} size={250} />
+                <Text style={styles.qrData}>{qrValue.guestName}</Text>
+                <Text style={styles.qrData}>
+                  {moment(qrValue.createdTime).format("DD/MMMM/YYYY")}
+                </Text>
+              </View>
+            </ViewShot>
+            <Button onPress={onCapture}>Compartir QR</Button>
+          </>
         ) : null}
       </View>
     </View>
@@ -63,8 +101,9 @@ const styles = StyleSheet.create({
   qrContainer: {
     marginTop: 20,
     alignItems: "center",
+    backgroundColor:'white'
   },
-  qrValue: {
+  qrData: {
     marginTop: 10,
     fontFamily: "monospace",
     fontSize: 12,
