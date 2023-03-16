93285caf2001d92b45c9a8f01c9e5282cec25c57
Memo Cachú <gcachu.o@gmail.com>
2023-03-16 14:00:14 -0600

    Add visits screen

--- screens/CreateTempVisitorQRScreen.tsx
+++ screens/CreateTempVisitorQRScreen.tsx
@@ -1,6 +1,6 @@
 import React, { useRef, useState } from "react";
-import { StyleSheet, View } from "react-native";
-import { Button, Text, TextInput } from "react-native-paper";
+import { StyleSheet, Text, View } from "react-native";
+import { Button, TextInput } from "react-native-paper";
 import * as Sharing from "expo-sharing";
 import QRCode from "react-native-qrcode-svg";
 import { createQRToken } from "../utils/qrUtils";
@@ -8,6 +8,9 @@ import { useAuth } from "../hooks/useAuth";
 import moment from "moment";
 import ViewShot from "react-native-view-shot";
 import BackButton from "../components/BackButton";
+import Constants from "expo-constants";
+import { useLinkTo } from "@react-navigation/native";
+import { doc, getFirestore, setDoc } from "firebase/firestore";
 
 const CreateTempVisitorQRScreen = () => {
   const captureQR = useRef<ViewShot>(null);
@@ -17,26 +20,44 @@ const CreateTempVisitorQRScreen = () => {
     guestName?: string;
     createdTime?: number;
   }>({});
+  const linkTo = useLinkTo();
 
   const isShown = qrValue.guestName;
+  const QRDeepLink = `${Constants.expoConfig?.scheme}://visitor/1`;
 
-  const handleGenerateQR = () => {
+  const handleGenerateQR = async () => {
     setQRValue({});
 
     if (!guestName) {
       return;
     }
 
+    const regex = /^[A-Za-zÀ-ÖØ-öø-ÿ\s]+$/; // Expresión regular para solo permitir letras y espacios
+    if (!regex.test(guestName)) {
+      setGuestName("");
+      alert("El nombre solo debe contener letras y espacios");
+      return;
+    }
+
     const qrData = {
       guestName: guestName,
+      hostAddress: "Mayorazgo San Jose #113",
       creatorUid: user?.uid,
       createdTime: new Date().getTime(),
       type: "unique",
       token: createQRToken(),
     };
-    setQRValue(qrData);
 
-    console.log(qrValue);
+    try {
+      await setDoc(doc(getFirestore(), "visits", qrData.token), qrData);
+      console.log("La visita ha sido registrada en Firestore.");
+      setQRValue(qrData);
+      console.log(qrValue);
+      const url = linkTo("/visitor/" + qrData.token);
+      console.log(url);
+    } catch (e) {
+      console.error("Error al registrar la visita:", e);
+    }
   };
 
   const onCapture = async () => {
@@ -53,51 +74,52 @@ const CreateTempVisitorQRScreen = () => {
   };
 
   return (
-    <View style={styles.container}>
+    <>
       <BackButton />
-      <View style={styles.form}>
-        <TextInput
-          label="Nombre del visitante"
-          value={guestName}
-          onChangeText={setGuestName}
-          style={styles.input}
-        />
-        <Button mode="contained" onPress={handleGenerateQR}>
-          Generar QR
-        </Button>
-        {isShown ? (
-          <>
-            <View
-              style={{
-                borderColor: "black",
-                borderStyle: "solid",
-                borderWidth: 1,
-                marginTop: 20,
-              }}
-            >
-              <ViewShot ref={captureQR}>
-                <View style={styles.qrContainer}>
-                  <Text style={styles.qrData}>Mayorazgo Santa Cecilia</Text>
-                  <QRCode value={"controlqr://myaccount/"} size={250} />
-                  <Text style={styles.qrData}>{qrValue.guestName}</Text>
-                  <Text style={styles.qrData}>
-                    {moment(qrValue.createdTime).format("DD/MMMM/YYYY")}
-                  </Text>
-                  <Text style={styles.qrData}>Un solo uso</Text>
-                </View>
-              </ViewShot>
-            </View>
-            <Button onPress={onCapture}>Compartir QR</Button>
-          </>
-        ) : null}
+      <View style={styles.container}>
+        <View style={styles.form}>
+          <TextInput
+            label="Nombre del visitante"
+            value={guestName}
+            onChangeText={setGuestName}
+            style={styles.input}
+          />
+          <Button mode="contained" onPress={handleGenerateQR}>
+            Generar QR
+          </Button>
+          {isShown ? (
+            <>
+              <View
+                style={{
+                  borderColor: "black",
+                  borderStyle: "solid",
+                  borderWidth: 1,
+                  marginTop: 20,
+                }}
+              >
+                <ViewShot ref={captureQR}>
+                  <View style={styles.qrContainer}>
+                    <Text style={styles.qrData}>Mayorazgo Santa Cecilia</Text>
+                    <QRCode value={QRDeepLink} size={250} />
+                    <Text style={styles.qrData}>{qrValue.guestName}</Text>
+                    <Text style={styles.qrData}>
+                      {moment(qrValue.createdTime).format("DD/MMMM/YYYY")}
+                    </Text>
+                    <Text style={styles.qrData}>Un solo uso</Text>
+                  </View>
+                </ViewShot>
+              </View>
+              <Button onPress={onCapture}>Compartir QR</Button>
+            </>
+          ) : null}
+        </View>
       </View>
-    </View>
+    </>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
     justifyContent: "center",
     alignItems: "center",
   },
@@ -118,7 +140,6 @@ const styles = StyleSheet.create({
   },
   qrData: {
     marginVertical: 10,
-    fontFamily: "monospace",
     fontSize: 13,
   },
 });
---
+++ screens/VisitorInfoScreen.tsx
@@ -0,0 +1,122 @@
+import React, { useEffect, useState } from "react";
+import { Alert, StyleSheet, Text, View } from "react-native";
+import { doc, getDoc, getFirestore } from "firebase/firestore";
+import { RouteProp, useNavigation, useRoute } from "@react-navigation/native";
+import moment from "moment";
+import BackButton from "../components/BackButton";
+
+interface VisitorInfoProps {
+  name: string;
+  address: string;
+  date: string;
+}
+
+const VisitorInfoScreen = () => {
+  const [isAuthorized, setIsAuthorized] = useState(false);
+  const [name, setName] = useState("");
+  const [address, setAddress] = useState("");
+  const [date, setDate] = useState("");
+  const [type, setType] = useState("");
+  const route = useRoute<RouteProp<Record<string, { id?: string }>, string>>();
+  const { id } = route.params;
+  const navigation = useNavigation();
+
+  useEffect(() => {
+    id && getVisitorById(id);
+  }, [id]);
+
+  const getVisitorById = async (id: string) => {
+    const docRef = doc(getFirestore(), "visits", id);
+    const docSnap = await getDoc(docRef);
+
+    if (docSnap.exists()) {
+      // el documento existe
+      const data = docSnap.data(); // devuelve los datos del documento
+
+      if (data) {
+        const name = data.guestName;
+        const address = data.hostAddress;
+        const date = data.createdTime;
+        let type = data.type;
+
+        switch (data.type) {
+          case "unique":
+            type = "Un solo uso";
+            break;
+        }
+
+        setName(name);
+        setAddress(address);
+        setType(type);
+        setDate(moment(date).format("DD [de] MMMM"));
+      }
+    } else {
+      // el documento no existe
+      console.log("No existe la visita con el ID:", id);
+      Alert.alert("El QR no es válido.");
+      navigation.goBack();
+    }
+  };
+
+  const handleAuthorize = () => {
+    // Aquí se realizaría la lógica para verificar si el visitante tiene autorización para ingresar
+    setIsAuthorized(true);
+  };
+
+  return (
+    <View>
+      <BackButton />
+      <View style={styles.container}>
+        <Text style={styles.title}>Información de visita</Text>
+        <View style={styles.infoContainer}>
+          <Text style={styles.label}>Nombre:</Text>
+          <Text style={styles.text}>{name}</Text>
+          <Text style={styles.label}>Dirección:</Text>
+          <Text style={styles.text}>{address}</Text>
+          <Text style={styles.label}>Fecha:</Text>
+          <Text style={styles.text}>{date}</Text>
+          <Text style={styles.label}>Tipo de acceso:</Text>
+          <Text style={styles.text}>{type}</Text>
+        </View>
+        {isAuthorized && (
+          <Text style={styles.authorizedText}>Ingreso autorizado</Text>
+        )}
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: "bold",
+    marginBottom: 20,
+  },
+  infoContainer: {
+    borderWidth: 1,
+    borderRadius: 10,
+    paddingVertical: 20,
+    paddingHorizontal: 50,
+    marginBottom: 20,
+  },
+  label: {
+    fontWeight: "bold",
+    marginBottom: 5,
+  },
+  text: {
+    marginBottom: 10,
+  },
+  button: {
+    marginVertical: 10,
+  },
+  authorizedText: {
+    fontSize: 18,
+    fontWeight: "bold",
+  },
+});
+
+export default VisitorInfoScreen;
